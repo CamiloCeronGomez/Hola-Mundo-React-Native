544bdb730739db99567132969d59d582a783e6ad
Camilo Ceron <camiloceron1990@gmail.com>
2020-07-30 00:25:00 -0500

    agregar imagen, boton, alerta y agrupar estilos

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 
 import React, {Component} from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Image, Button, Alert} from 'react-native'
 
 class holaCiapfa extends Component{
+
+	saludo = () => {Alert.alert('Mensaje de alerta Login')}
 
 	render(){
 
@@ -12,14 +14,16 @@ class holaCiapfa extends Component{
 				<View style ={styles.header}>
 
 					<View style={styles.headerleft}>
+					<Image source={require('./assets/CAMILO.png')} style ={styles.logo}/>
 					</View>
 
 					<View style={styles.headerright}>
+					<Button title='Login' onPress={this.saludo}/>
 					</View>
 
 				</View>
 
-				<View style={styles.body}>
+				<View style={[styles.body, styles.negrita]}>
 				<Text>hola mundo react native paso a paso</Text>
 				</View>
 
@@ -35,21 +39,32 @@ const styles = StyleSheet.create({
 		flex : 1,
 		flexDirection : 'column'
 	}, 
+	negrita :{
+		fontWeight : 'bold'
+	},
 	header:{
 		flex : 0.3,
-		flexDirection : 'row'
+		flexDirection : 'row',
+		marginTop: 40
 	},
 	headerleft:{
 		flex : 1,
-		backgroundColor : 'yellow'
+		
 	},
 	headerright:{
 		flex : 1,
-		backgroundColor : 'blue'
+	
 	},
 	body:{
 		flex : 1,
-		backgroundColor : 'red'
+		alignItems : 'center'
+		
+	},
+	logo : {
+		width : 100,
+		height : 100,
+		borderRadius: 50,
+		resizeMode: 'contain'
 	}
 
 })
